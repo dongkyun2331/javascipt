b68d545246f1982c0c1495cfe31b89117eaf8478
dongkyun2331 <dongkyun2331@gmail.com>
2023-01-10 15:58:42 +0900

    a

---
+++ tutorial/06_scroll effect/app.js
@@ -0,0 +1,16 @@
+const bg = document.querySelector('.bg')
+const title = document.querySelector('.title')
+
+window.addEventListener('scroll', function () {
+  const value = window.scrollY
+
+  title.classList.add('active')
+
+  if (value == 0) {
+    title.classList.remove('active')
+  } else {
+    title.classList.add('active')
+  }
+
+  bg.style.clipPath = `circle(${value}px at 50% 50%)`
+})
